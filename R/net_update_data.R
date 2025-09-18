6cc57761b749701442ff6a0d05e299f17761d89f
Radu Diaconescu <r.diaconescu@stud.uni-goettingen.de>
2025-09-18 22:32:28 +0200

    fixed name

--- R/net_update_data.R
+++ R/net_update_data.R
@@ -3,7 +3,7 @@ net_update_data <- function(data) {
   new.data <- c()
   for(sample_id in unique(data$sample_processing_id)){
     data.1 <- data[data$sample_processing_id == sample_id,]
-    new.data.1 <- compute_sparse_hamming_distance_matrix_by_length(data.1, sample_id)
+    new.data.1 <- compute_dist_matrix(data.1, sample_id)
     new.data <- rbind(new.data, new.data.1)
   }
   return(new.data)
