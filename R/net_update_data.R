8b425ac6f4d37522b8026af9fb662ea15c322fb2
Radu Diaconescu <r.diaconescu@stud.uni-goettingen.de>
2025-09-20 16:47:39 +0200

    added sim_method to function parameters

--- R/net_update_data.R
+++ R/net_update_data.R
@@ -1,9 +1,9 @@
-net_update_data <- function(data) {
+net_update_data <- function(data, sim_method="HAMMING") {
   # sample_size=2 # REMOVE IN PKG
   new.data <- c()
   for(sample_id in unique(data$sample_processing_id)){
     data.1 <- data[data$sample_processing_id == sample_id,]
-    new.data.1 <- compute_dist_matrix(data.1, sample_id)
+    new.data.1 <- compute_dist_matrix(data.1, sample_id, sim_method)
     new.data <- rbind(new.data, new.data.1)
   }
   return(new.data)
