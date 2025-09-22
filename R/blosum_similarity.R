353541656b0e5247477ab3f408e87957f03f181e
Radu Diaconescu <r.diaconescu@stud.uni-goettingen.de>
2025-09-22 11:11:01 +0200

    deleted unnecessary comment

--- R/blosum_similarity.R
+++ R/blosum_similarity.R
@@ -7,8 +7,6 @@
 #'
 blosum_similarity <- function(vj_data) {
 
-  #random meaningless comment!!!!
-
   #Check whether data is appropriate
   if (!(nrow(vj_data) > 1)) {
     stop("At least two sequences needed to calculate similarity.")
